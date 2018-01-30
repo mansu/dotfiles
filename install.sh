c00a9f39d81d9a5aa4aff58f2530280676a068c1
Suman Karumuri <skarumuri@pinterest.com>
2018-01-30 14:36:01 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,11 @@ cp -v zsh/.zshrc ~/.zshrc
 cp -v zsh/.zshrc_common ~/.zshrc_common
 cp -v zsh/.zshrc_local ~/.zshrc_local
 
+# Mac
 brew install ccat
 brew install ag
 # CLI for managing mac
 brew install mas m-cli
+
+# Linux
+apt-get install silversearcher-ag atop 
